dd58d3b7b7e0a826ab5105b8f7281008eee0d533
Torsten Grust <torsten.grust@gmail.com>
2025-07-17 12:30:41 +0200

    Publish Haskell material for Chapter 06

---
+++ material/06/sequence-NonDet.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TupleSections #-}
+
+type NonDet a = [a]
+
+(>=>) :: (a -> NonDet b) -> (b -> NonDet c) -> (a -> NonDet c)
+f >=> g = \x -> concat [ g y | y <- f x ]
+
+
+oneOf :: [a] -> NonDet a
+oneOf xs = xs
+
+culDeSac :: NonDet a
+culDeSac = []
+
+
+multiplyTo :: Integer -> NonDet (Integer, Integer)
+multiplyTo m = (const (oneOf [1..m]) >=>
+                 (\x -> map (x,) (oneOf [1..m])) >=>
+                   \(x,y) -> if x * y == m then [(x,y)] else culDeSac) $ m
+
+main :: IO ()
+main = print $ multiplyTo 32
